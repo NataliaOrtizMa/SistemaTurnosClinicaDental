f4d8fef65f689d037ebe876b731a76fa20256376
NataliaOrtizMa <nataliaortizma@gmail.com>
2022-07-08 20:24:14 -0500

    Create excepciones files

---
+++ src/main/java/com/finalproject/sistemaTurnosClinicaDental/excepciones/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.finalproject.sistemaTurnosClinicaDental.excepciones;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
